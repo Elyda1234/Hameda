629155555f135fcde88cb48e5dd122f8a318b903
Elyda1234 <yuliantielida52@gmail.com>
2025-05-08 17:24:06 +0700

    Add files via upload

---
+++ aktivitas.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/styleumum.css">
+</head>
+<body>
+    <!-- Bagian Navbar -->
+    <nav class="Header">
+        <ul class="Header-Bar">
+            <li><a href="indexumum.php">Home</a></li>
+            <li><a href="aktivitas.php">Aktivitas</a></li>
+            <li><a href="hobi.php">Hobi</a></li>
+            <li><a href="blog.php">Blog</a></li>
+            
+        </ul>
+    </nav>
+
+    <!-- Bagian Pembagian Kolom -->
+    <div class="Isi">
+        <div class="Kolom-Kiri">
+            <h3 li><a href="haripertamacoding.php">1. aktivitas 1 </a></li></h3>
+            <div class="TeksGambaraktivitas">
+                <img src="assets/css/badminton.jpg" alt="Deskripsi Gambar" class="Gambaraktivitas">
+            </div>
+            <p>pernah jadi panitia badminton
+            </p>
+            <h3 li><a href="harikeduacoding.php">2. akivitas 2</a></li></h3>
+            <div class="TeksGambaraktivitas">
+                <img src="assets/css/vc.jpg" alt="Deskripsi Gambar" class="Gambaraktivitas">
+            </div>
+            <p>jadi panitia vocal group dan panitia body worship</p>
+
+            <h3 li><a href="hariketigacoding.php">3. aktivitas 3</a></li></h3>
+            <div class="TeksGambaraktivitas">
+                <img src="assets/css/voli.jpg" alt="Deskripsi Gambar" class="Gambaraktivitas">
+            </div>
+            <p>pernah jadi panitia voli PDN 2025</p>
+    
+            <h3 li><a href="harikeempatcoding.php">4. aktivitas 4</a></li></h3>
+            <div class="TeksGambaraktivitas">
+                <img src="assets/css/cca.jpg" alt="Deskripsi Gambar" class="Gambaraktivitas">
+            </div>
+            <p>pernah jadi panitia cca</p>
+            
+            <h3 li><a href="harikelimacoding.php">5.  aktivitas 5</a></li></h3>
+            <div class="TeksGambaraktivitas">
+                <img src="assets/css/paskah.jpg" alt="Deskripsi Gambar" class="Gambaraktivitas">
+            </div>
+            <p>Diberi kepercayan sebagai koordinator(co) konsumsi dan usah pada perayaan paskah matematika tahun 2025</p>
+
+            <h3 li><a href="harikeenamcoding.php">6. aktivitas 6</a></li></h3>
+            <div class="TeksGambaraktivitas">
+                <img src="assets/css/konus.jpg" alt="Deskripsi Gambar" class="Gambaraktivitas">
+            </div>
+            <p>Diberikan kesempatan juga untuk menjadi koordinator konsumsi dan usaha untuk acara pmo odim matematika tahun 2025</p>
+            
+
+            <h3 li><a href="hariketujuhcoding.php">7. aktivitas 7</a></li></h3>
+            <div class="TeksGambaraktivitas">
+                <img src="assets/css/panitia.jpg" alt="Deskripsi Gambar" class="Gambaraktivitas">
+            </div>
+            <p>poto bersama panitia panitia kece </p>
+        </div>
+        <div class="Kolom-Kanan-Semuaaktivitas">
+            <div class="Kolom-Kananhobi">
+               <h3 li><a href="hobi.php">Hobi</h3>
+                <img src="assets/css/hobigambar.jpg" alt="Deskripsi" class="Gambaraktivitashobi">
+                <div class="Bagianaktivitashobi">
+                <p li><a href="game.php">  1. Bermain Game</a></li><br>
+                <p li><a href="gambar.php"> 2. Menggambar</a></li><br>
+                </p>
+                </div>
+            </div>
+           <div class="Kolom-Kananhobi">
+                <div class="BlogSidebar">
+                    <h3 li><a href="blog.php">Blog</h3>
+                    <img src="assets/css/blog.jpg" alt="Deskripsi" class="Gambaraktivitashobi">
+                    <div class="SidebarItem"><a href="blog-detail.php">Artikel 1: PHP Basics</a></div>
+                    <div class="SidebarItem"><a href="blog-detail2.php">Artikel 2: HTML & CSS Guide</a></div>
+                    <div class="SidebarItem"><a href="blog-detail3.php">Artikel 3: Website Structure</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="Footer">
+        Copyright &copy; 2025
+    </div>
+</body>
+</html>
---
+++ indexumum.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/styleumum.css">
+</head>
+<body>
+    <!-- Bagian Navbar -->
+    <nav class="Header">
+        <ul class="Header-Bar">
+              <li><a href="index.php">Home</a></li>
+            <li><a href="aktivitas.php">Aktivitas</a></li>
+            <li><a href="hobi.php">Hobi</a></li>
+            <li><a href="blog.php">Blog</a></li>
+        </ul>
+    </nav>
+
+    <!-- Bagian Pembagian Kolom -->
+    <div class="Isi">
+        <div class="Kolom-Kiriindex">
+            <h3 class="Teks">Selamat Datang Pengunjung</h3>
+            <div class="TeksGambaraktivitashari1">
+                <img src="assets/css/elii.jpg" alt="Deskripsi Gambar" class="Gambarindexumum">
+            <p class="teksgameumum"> 
+                <i>Website ini dibuat oleh Elyda Yulianti Panjaitan sebagai bagian dari tugas mata kuliah Komputer dan Pemrograman. Melalui website ini saya ingin menjadikan sebagai ruang terbuka untuk menampilkan hasil belajar, informasi pribadi, serta kegiatan kegiatan saya yang lain.</i> 
+            </p>
+            </div>
+        </div>
+        <div class="Kolom-Kanan-Semua">
+            <div class="Kolom-Kananindex">
+                <h3 li><a href="biodata.php">Biodata</a></li></h3>
+                <img src="assets/css/li.jpg" alt="Deskripsi Gambar" class="Gambarbiodataindex"> 
+                <p>Biodata Pembuat Website</p>
+            </div>
+            <div class="Kolom-Kananindex2">
+                 <h3 li><a href="aktivitas.php">Aktivitas</a></li></h3>
+                <img src="assets/css/eltok.jpg" alt="Deskripsi Gambar" class="Gambarrr">
+                <p>seseorang yang bernama elydaa yang suka kulineran, masak taoi gak jago jago banget </p>
+            </div>
+        </div>
+    </div>
+    <div class="Footer">
+        Copyright &copy; 2025
+    </div>
+</body>
+</html>
